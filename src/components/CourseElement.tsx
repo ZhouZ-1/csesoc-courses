0146180a71e54d34e8fd33a774118e3ae9e84acf
ZhouZ <16379978+ZhouZ-1@users.noreply.github.com>
2022-09-05 20:30:27 +1000

    Use correct semantic element

--- src/components/CourseElement.tsx
+++ src/components/CourseElement.tsx
@@ -4,14 +4,14 @@ interface Props {
 
 const CourseElement = ({ course }: Props) => {
   return (
-    <a
+    <button
       className="text-xl basis-1/3 sm:basis-1/4 mt-1 text-center font-mono cursor-pointer 2xl:text-2xl select-none"
       onClick={async () =>
         await navigator.clipboard.writeText(`/role give role:@${course}`)
       }
     >
       {course}
-    </a>
+    </button>
   );
 };
 

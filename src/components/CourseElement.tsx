367875e959c023995b72517134eb56f54b679c57
ZhouZ <16379978+ZhouZ-1@users.noreply.github.com>
2022-09-05 20:09:24 +1000

    Revert large font size in small screen

--- src/components/CourseElement.tsx
+++ src/components/CourseElement.tsx
@@ -5,7 +5,7 @@ interface Props {
 const CourseElement = ({ course }: Props) => {
   return (
     <a
-      className="lg:text-xl basis-1/3 sm:basis-1/4 mt-1 text-center font-mono cursor-pointer 2xl:text-2xl text-2xl"
+      className="text-xl basis-1/3 sm:basis-1/4 mt-1 text-center font-mono cursor-pointer 2xl:text-2xl"
       onClick={() =>
         navigator.clipboard.writeText(`/role give role:@${course}`)
       }
